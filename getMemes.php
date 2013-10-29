66bebe6b419e59ab0c12df47c2435521f3420e48
sijp <sijproject@gmail.com>
2013-10-29 12:31:52 +0200

    getMemes now returns a json

--- getMemes.php
+++ getMemes.php
@@ -4,4 +4,12 @@ require_once dirname(__FILE__)."/MemesHandler.php";
 
 $handler=new MemesHandler();
 
-print_r($handler->getMemes());
+$dict=$handler->getMemes();
+$jsonable=array();
+foreach ($record as $dict){
+	array_append(	$jsonable,
+			array(	"id"=>$record["id"],
+				"description"=>$record["nmemeame"]));
+}
+
+echo json_encode($jsonable);
